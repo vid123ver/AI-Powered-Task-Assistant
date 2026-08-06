bbe39070e3d9449c32e995aa70579f14f7edcdef
vidhan_verma <vidhanverma2311@gmail.com>
2026-08-06 21:19:50 +0530

    Gemini -> request a tool

--- backend/src/services/geminiService.ts
+++ backend/src/services/geminiService.ts
@@ -1,5 +1,6 @@
 import geminiClient from "../config/gemini";
 import { taskTools } from "../ai/tools";
+import * as taskService from "./taskService";
 class GeminiService {
   async sendMessage(message: string): Promise<string> {
     try {
@@ -14,6 +15,15 @@ class GeminiService {
     ],
   },
 });
+const functionCall =
+  response.candidates?.[0]?.content?.parts?.[0]?.functionCall;
+
+console.log("Function Call:", functionCall);
+if (functionCall?.name === "list_tasks") {
+  const tasks = await taskService.findAll();
+
+  console.log("Tasks:", tasks);
+}
     console.log(JSON.stringify(response, null, 2));
       return response.text ?? "No response received from Gemini.";
     } catch (error) {
